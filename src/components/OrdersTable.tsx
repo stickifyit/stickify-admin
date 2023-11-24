2d6b2586d29c9b29a7acedee111dfe19ca25c044
tchisama <tchisama41@gmail.com>
2023-11-24 22:45:11 +0100

    t-shirt api

--- src/components/OrdersTable.tsx
+++ src/components/OrdersTable.tsx
@@ -9,19 +9,80 @@ import {
     TableHeader,
     TableRow,
   } from "@/src/components/ui/table"
+import { useCurrentContainer } from "@/store/currentContainer"
+import axios from "axios"
+import { Coffee, Shirt, Sticker } from "lucide-react"
+import { useEffect, useState } from "react"
+
+
+
+
+const servicesIcons = {
+    "sticker": <Sticker/>,
+    "t-shirt": <Shirt/>,
+    "cup": <Coffee/>,
+    "label": <Sticker/>
+}
+
+
+
+type Sticker = {
+    design: string;
+    type: string;
+    size: string;
+    _id: string;
+  };
   
-  const invoices = [
-    {
-        image: "https://storage.googleapis.com/stickify-storage/uploads/8x1oppywrts-3hfofna68ga-mba44fkfjcn.png",
-        quantity: 1,
-        number: "0689978614",
-        serviceType:"Sticker",
-        state:"current",
-        user:"abdessamade zalmadi",
-    },
-  ]
+  type TShirt = {
+    design: string;
+    type: string;
+    size: string;
+    _id: string;
+  };
   
+  type Label = {
+    design: string;
+    type: string;
+    size: string;
+    _id: string;
+  };
+  
+  type Cup = {
+    design: string;
+    type: string;
+    size: string;
+    _id: string;
+  };
+  
+  type Order = {
+    _id: string;
+    customerId: string;
+    number: string;
+    email: string;
+    fullName: string;
+    address: string;
+    quantity: number;
+    serviceType: 'sticker' | 't-shirt' | 'label' | 'cup';
+    sticker?: Sticker; // optional for serviceType 'sticker'
+    "t-shirt"?: TShirt;   // optional for serviceType 't-shirt'
+    label?: Label;     // optional for serviceType 'label'
+    cup?: Cup;         // optional for serviceType 'cup'
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+  };
+   
   export function OrdersTable() {
+    const {reload} = useCurrentContainer()
+    const [orders,setOrders] = useState<Order[]>([])
+
+    useEffect(()=>{
+        axios.get<Order[]>("http://localhost:3001/orders/all").then(res=>{
+            setOrders(res.data)
+            console.log(res.data)
+        })
+    },[reload])
+
     return (
     <>
         <h1 className='text-4xl py-6'>Orders</h1>
@@ -31,26 +92,30 @@ import {
                 <TableHeader>
                 <TableRow>
                     <TableHead className="w-[100px]">Sticker</TableHead>
+                    <TableHead>service</TableHead>
                     <TableHead>User</TableHead>
                     <TableHead>Number</TableHead>
-                    <TableHead>service</TableHead>
+                    <TableHead>Address</TableHead>
                     <TableHead>quantity</TableHead>
                     <TableHead className="text-right">state</TableHead>
                 </TableRow>
                 </TableHeader>
                 <TableBody>
-                {invoices.map((invoice,i) => (
+                {orders.map((order,i) => {
+                   const service = order.serviceType 
+                    return(
                     <TableRow key={i}>
                     <TableCell className="font-medium">
-                        <img src={invoice.image} width={50} alt="" />
+                        <img src={"https://storage.googleapis.com/stickify-storage/"+order[service as "sticker" | "t-shirt" | "label" | "cup"]?.design ?? ' '} width={50} alt="" />
                     </TableCell>
-                    <TableCell>{invoice.user}</TableCell>
-                    <TableCell>{invoice.number}</TableCell>
-                    <TableCell>{invoice.serviceType}</TableCell>
-                    <TableCell>{invoice.quantity}</TableCell>
-                    <TableCell className="text-right">{invoice.state}</TableCell>
+                    <TableCell>{servicesIcons[order.serviceType]}</TableCell>
+                    <TableCell>{order.fullName}</TableCell>
+                    <TableCell>{order.number}</TableCell>
+                    <TableCell>{order.address}</TableCell>
+                    <TableCell>{order.quantity} item{order.quantity>1 && "s"}</TableCell>
+                    <TableCell className="text-right">{"ready"}</TableCell>
                     </TableRow>
-                ))}
+                )})}
                 </TableBody>
             </Table>
         </Card>
